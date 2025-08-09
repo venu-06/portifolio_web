3c1ca58fd9a7440d9d5afc0ac10b19b444309e3b
veenasahithi2004 <23501a4436@pvpsit.ac.in>
2025-08-10 00:32:12 +0530

    Delete app1.py

--- app1.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-# Read your portfolio HTML file
-with open("portifolio.html", "r", encoding="utf-8") as f:
-    portfolio_html = f.read()
-
-# Display HTML inside Streamlit
-st.components.v1.html(portfolio_html, height=3000, scrolling=True)
